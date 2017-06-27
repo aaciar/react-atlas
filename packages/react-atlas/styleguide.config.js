7ddb39877cd87b55244899478e1070199bf58b2f
U-MPLS\hbowden <2trill2spill@gmail.com>
2017-06-27 10:50:57 -0500

    Add Table and TableHeader to styleguidist.

--- packages/react-atlas/styleguide.config.js
+++ packages/react-atlas/styleguide.config.js
@@ -10,10 +10,10 @@ module.exports = {
 
   // Use this to test a single component.  Change it to the component you are testing and restart the styleguide server
   // Regex should be: 'src/components/NAME_OF_COMPONENT_FOLDER/[A-Z]*.js'
-  components: 'src/components/**/[Button|Avatar|Card|Dropdown]*.js',
+  components: 'src/components/**/[Button|Avatar|Card|Dropdown|Table|TableHeader]*.js',
   contextDependencies: [
     '../react-atlas-core/src',
-      '../react-atlas-theme/src'
+    '../react-atlas-theme/src'
   ],
   defaultExample: true,
   webpackConfig: {
